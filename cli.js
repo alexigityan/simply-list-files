e6254376cb24245ce4f6853d8e1a6eec1a496f7b
Alex <alexigityan@yahoo.com>
2019-07-24 14:31:20 +0400

    initial commit

---
+++ cli.js
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+
+const currentPath = process.cwd();
+
+let numOfFiles = 0;
+let numOfDirs = 0;
+
+let log = console.log;
+
+const [,, flag] = process.argv;
+if (flag) {
+  if (flag === '--help') {
+    console.log('This program will show all the files from current and nested folders.');
+    console.log('The result is shown in console by default.');
+    console.log('Alternatively, add --f to create a log file instead (that\'s faster).');
+    console.log('Have fun!');
+    process.exit(0);
+  } else if (flag === '--f') {
+    const fileName = `log-${Date.now()}.txt`;
+    console.log(`Results will be written in file ${fileName}`);
+    const ws = fs.createWriteStream(fileName);
+    function logger(stream) {
+      return function(text) {
+        stream.write(text + '\n');
+      }
+    }
+    log = logger(ws);
+  } else {
+    throw new Error('unknown argument provided: ' + flag);
+  }
+}
+
+function findAllFiles(dirPath) {
+  fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+    if (err) throw(err);
+    
+    files.forEach(file => {
+      const filePath = path.join(dirPath, file.name);
+      if (file.isDirectory()) {
+        numOfDirs++;
+        findAllFiles(filePath);
+      } else {
+        numOfFiles++;
+        log(filePath);
+      }
+    });
+  });
+}
+
+findAllFiles(currentPath);
+
+process.on('exit', code => {
+  if (code === 0) {
+    const subdirText = numOfDirs ? ` in ${numOfDirs} subdirectories` : '';
+    console.log(`Done! Found ${numOfFiles} files` + subdirText);
+    console.log(`That took ${process.uptime()} seconds`);
+  }
+});
